af49c19fb252c297d17a861ac84cb7be74a1f740
Omar <lordbida55@gmail.com>
2022-12-15 03:21:07 +0100

    some enhancments

--- code.js
+++ code.js
@@ -1,11 +1,25 @@
 let _sec = document.getElementById('sec')
 let _min = document.getElementById('min')
 let _mlsec = document.getElementById('mlsec')
-
+let _checkpoints = document.getElementById('checkpoints')
 let mlsec = 0
 let sec = 0
 let min = 0
 let startCountUp
+let runing = false
+
+const save =
+  '<div class="row checkpoints col-12">\
+    <div class="col-2 save">$$min</div>\
+    <div class="separat col-1 save">:</div>\
+    <div class="col-2 save">$$sec</div>\
+    <div class="separat col-1 save">:</div>\
+    <div class="col-2 save">$$mlsec</div>\
+  </div>'
+
+show('start')
+hide('stop')
+
 function startcount() {
   return setInterval(() => {
     mlsec += 1
@@ -26,17 +40,31 @@ let cleared = true
 document.getElementById('stop').onclick = function () {
   clearInterval(startCountUp)
   cleared = true
+  runing = false
+  show('start')
+  hide('stop')
 }
 document.getElementById('start').onclick = function () {
   startCountUp = startCountUp && !cleared ? startCountUp : startcount()
   cleared = false
+  runing = true
+  show('stop')
+  hide('start')
 }
 document.getElementById('reset').onclick = function () {
   mlsec = 0
   sec = 0
   min = 0
-
+  _checkpoints.innerHTML = ''
   screenUpdate()
+}
+document.getElementById('save').onclick = function () {
+  if (runing) {
+    _checkpoints.innerHTML += save
+      .replaceAll('$$min', min)
+      .replaceAll('$$sec', sec)
+      .replaceAll('$$mlsec', mlsec)
+  }
 }
 
 function screenUpdate() {
@@ -67,4 +95,11 @@ function screenUpdate() {
       _min.innerHTML = '0' + min
     }
   }
+}
+
+function show(id) {
+  document.getElementById(id).style.display = 'block'
+}
+function hide(id) {
+  document.getElementById(id).style.display = 'none'
 }
